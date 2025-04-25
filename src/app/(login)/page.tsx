49057e0b0970998f8330e2b3272a85bbf25b9200
Antonio Marcelo Barreto <antoniomarcelob12@gmail.com>
2025-04-25 18:06:01 -0300

    feat: add login-screen

---
+++ src/app/(login)/page.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function RegisterPage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-background">
+            <div className="w-full max-w-md p-6">
+                <div className="flex flex-col items-center space-y-2 mb-6">
+                    <div className="size-36 bg-primary rounded-full flex items-center justify-center">
+                        <Image src='/logo.png' alt="logo" width={100} height={100} />
+                    </div>
+                    <h1 className="text-3xl font-bold text-primary">Sistema de Gestão</h1>
+                </div>
+
+                <Card>
+                    <CardHeader>
+                        <CardTitle className="text-2xl text-center">Login</CardTitle>
+                        <CardDescription className="text-center">Acesse no sistema com suas credenciais.</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <form>
+                            <div className="grid gap-4">
+                                <div className="grid gap-2">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input id="email" type="email" placeholder="exemplo@empresa.com" />
+                                </div>
+                                <div className="grid gap-2">
+                                    <Label htmlFor="password">Senha</Label>
+                                    <Input id="password" type="password" />
+                                </div>
+                            </div>
+                        </form>
+                    </CardContent>
+                    <CardFooter className="flex flex-col space-y-4">
+                        <Button className="w-full" asChild>
+                            <Link href="/dashboard">Entrar</Link>
+                        </Button>
+                    </CardFooter>
+                </Card>
+            </div>
+        </div>
+    );
+}
+
