d631a9447fd4ec0360d066ab54c42ca6ce41c33b
Antonio Marcelo Barreto <antoniomarcelob12@gmail.com>
2025-04-25 17:31:42 -0300

    initial configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,24 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.config({
+        extends: ["next/core-web-vitals", "next/typescript"],
+        rules: {
+            "semi": ["warn", "always"],
+            "indent": ["error", 4],
+            "quotes": ["error", "double"],
+            "no-trailing-spaces": "error",
+            "no-mixed-spaces-and-tabs": "error",
+            "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],  // Ignora args que começam com _
+
+            "camelcase": ["error", { properties: "always" }],
+            "no-multiple-empty-lines": ["error", { max: 1 }],
+        }
+    }),
 ];
 
 export default eslintConfig;
