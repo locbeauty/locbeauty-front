9ca83d7004c0fe511018919730f3aae2dda80ccf
Antonio Marcelo Barreto <antoniomarcelob12@gmail.com>
2025-05-12 00:40:03 -0300

    fix: bookings, employees and gears table mock data changed

--- src/components/pages/gears/view/GearsTable.tsx
+++ src/components/pages/gears/view/GearsTable.tsx
@@ -1,41 +1,74 @@
+import { Check, X } from "lucide-react";
+
 export function GearsTable() {
     return (
         <div className="border rounded-lg max-h-[70vh] lg:w-full w-[89vw] overflow-x-auto">
             <table className="w-full">
                 <thead className="bg-muted">
                     <tr>
+                        <th className="text-left p-3 font-medium">Nome</th>
                         <th className="text-left p-3 font-medium">Descrição</th>
-                        <th className="text-left p-3 font-medium">Peso</th>
-                        <th className="text-left p-3 font-medium">Altura</th>
-                        <th className="text-left p-3 font-medium">Porte</th>
-                        <th className="text-left p-3 font-medium">Região</th>
-                        <th className="text-left p-3 font-medium">Quantidade</th>
+                        <th className="text-left p-3 font-medium">Regional</th>
+                        <th className="text-left p-3 font-medium">Unidades disponíveis</th>
+                        <th className="text-left p-3 font-medium">Unidades totais</th>
+                        <th className="text-left p-3 font-medium">Data da aquisição</th>
+                        <th className="text-left p-3 font-medium">Pode ser transferido?</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr className="border-t hover:bg-muted/50">
-                        <td className="p-3">Escavadeira Hidráulica</td>
-                        <td className="p-3">20 ton</td>
-                        <td className="p-3">3.5 m</td>
-                        <td className="p-3">Grande</td>
-                        <td className="p-3">Sudeste</td>
-                        <td className="p-3">5</td>
+                        <td className="p-3">Lavieen</td>
+                        <td className="p-3">Lorem ipsum dolor sit amet. Qui quia quas eum explicabo consequuntur qui esse deserunt.</td>
+                        <td className="p-3">Pernambuco</td>
+                        <td className="p-3 text-center">5</td>
+                        <td className="p-3 text-center">10</td>
+                        <td className="p-3 text-center">20/01/2023</td>
+                        <td className="p-3 flex justify-center"><Check /></td>
                     </tr>
                     <tr className="border-t hover:bg-muted/50">
-                        <td className="p-3">Compressor de Ar</td>
-                        <td className="p-3">150 kg</td>
-                        <td className="p-3">0.8 m</td>
-                        <td className="p-3">Médio</td>
-                        <td className="p-3">Sul</td>
-                        <td className="p-3">12</td>
+                        <td className="p-3">UF3</td>
+                        <td className="p-3">Lorem ipsum dolor sit amet. Qui quia quas eum explicabo consequuntur qui esse deserunt.</td>
+                        <td className="p-3">Pernambuco</td>
+                        <td className="p-3 text-center">5</td>
+                        <td className="p-3 text-center">10</td>
+                        <td className="p-3 text-center">20/01/2023</td>
+                        <td className="p-3 flex justify-center"><X /></td>
                     </tr>
                     <tr className="border-t hover:bg-muted/50">
-                        <td className="p-3">Furadeira Industrial</td>
-                        <td className="p-3">5 kg</td>
-                        <td className="p-3">0.3 m</td>
-                        <td className="p-3">Pequeno</td>
-                        <td className="p-3">Nordeste</td>
-                        <td className="p-3">25</td>
+                        <td className="p-3">Herus Hifu</td>
+                        <td className="p-3">Lorem ipsum dolor sit amet. Qui quia quas eum explicabo consequuntur qui esse deserunt.</td>
+                        <td className="p-3">Bahia</td>
+                        <td className="p-3 text-center">5</td>
+                        <td className="p-3 text-center">10</td>
+                        <td className="p-3 text-center">20/01/2023</td>
+                        <td className="p-3 flex justify-center"><Check /></td>
+                    </tr>
+                    <tr className="border-t hover:bg-muted/50">
+                        <td className="p-3">Ultraformer</td>
+                        <td className="p-3">Lorem ipsum dolor sit amet. Qui quia quas eum explicabo consequuntur qui esse deserunt.</td>
+                        <td className="p-3">Rio de Janeiro</td>
+                        <td className="p-3 text-center">5</td>
+                        <td className="p-3 text-center">10</td>
+                        <td className="p-3 text-center">20/01/2023</td>
+                        <td className="p-3 flex justify-center"><Check /></td>
+                    </tr>
+                    <tr className="border-t hover:bg-muted/50">
+                        <td className="p-3">Delight</td>
+                        <td className="p-3">Lorem ipsum dolor sit amet. Qui quia quas eum explicabo consequuntur qui esse deserunt.</td>
+                        <td className="p-3">Ceará</td>
+                        <td className="p-3 text-center">5</td>
+                        <td className="p-3 text-center">10</td>
+                        <td className="p-3 text-center">20/01/2023</td>
+                        <td className="p-3 flex justify-center"><X /></td>
+                    </tr>
+                    <tr className="border-t hover:bg-muted/50">
+                        <td className="p-3">Lightsheer Duet</td>
+                        <td className="p-3">Lorem ipsum dolor sit amet. Qui quia quas eum explicabo consequuntur qui esse deserunt.</td>
+                        <td className="p-3">Ceará</td>
+                        <td className="p-3 text-center">5</td>
+                        <td className="p-3 text-center">10</td>
+                        <td className="p-3 text-center">20/01/2023</td>
+                        <td className="p-3 flex justify-center"><Check /></td>
                     </tr>
                 </tbody>
             </table>
