a095b015c13249fc2a2b8dd1368aa907f3335b7a
Antonio Marcelo <antoniomarcelob12@gmail.com>
2025-08-14 09:45:17 -0300

    add next url in login page request

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -35,7 +35,7 @@ export default function LoginPage() {
     });
 
     async function handleLogin({ documentNumber, password }: LoginSchemaType) {
-        const res = await fetch("/api/login", {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/login`, {
             method: "POST",
             credentials: "include",
             headers: {
